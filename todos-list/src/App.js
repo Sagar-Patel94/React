935db605f2e102db73e23d38591d6a04b9d06810
Sagar-Patel94 <s.m.gajera.sg@gmail.com>
2022-08-27 16:45:52 +0530

    27/08/2022 Saturday 04:45 PM

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -2,9 +2,19 @@ import "./App.css";
 import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { Footer } from "./MyComponents/Footer";
+import React, { useState } from 'react';
 
 function App() {
-  let todos = [
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+    setTodos(todos.filter((e) => {
+      return e!==todo;
+    }));
+  }
+  const [todos, setTodos] = useState([
     {
       sno: 1,
       title: "Go to the market",
@@ -16,15 +26,15 @@ function App() {
       desc: "You need to go to the market to get this job done2"
     },
     {
-      sno: 1,
+      sno: 3,
       title: "Go to the ghat",
       desc: "You need to go to the market to get this job done3"
     }
-  ]
+  ]);
   return (
     <>
       <Header title="My Todos List" searchBar={false}/>
-      <Todos todos={todos}/>
+      <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </>
   );
--- todos-list/src/MyComponents/Todos.js
+++ todos-list/src/MyComponents/Todos.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import {TodoItem} from "./TodoItem";
+import React from "react";
+import { TodoItem } from "./TodoItem";
 
 export const Todos = (props) => {
   return (
-    <div className='container'>
-      <h3>Todos List</h3>
-      {/* {props.todos} */}
-      <TodoItem todo={props.todos[0]}/>
+    <div className="container">
+      <h3 className="text-center my-3">Todos List</h3>
+      {props.todos.map((todo) => {
+        return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />;
+      })}
     </div>
-  )
-}
+  );
+};
