36814629f569290d240f2f263f7cb89b5d98e3d7
Sagar-Patel94 <s.m.gajera.sg@gmail.com>
2022-08-28 15:45:14 +0530

    28/08/2022 Sunday 03:45 PM

--- todos-list/src/MyComponents/Footer.js
+++ todos-list/src/MyComponents/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>footer works!!</div>;
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">Copyright &copy; MyTodosList.com</p>
+    </footer>
+  );
 };
